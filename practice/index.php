45b8e29243febd871b87222a9eaa4e18408ad362
Faisal <faisalahmed4315@gmail.com>
2024-11-01 19:50:33 +0600

    advance

--- practice/index.php
+++ practice/index.php
@@ -78,8 +78,56 @@
     $names = NULL;
     $myName = "Faisal";
     echo $names ?? $myName;
+    echo "</br>";
 
-    
+    //condition if else
+    $myAge = 24;
+    if ($myAge < 18) {
+        echo "you are a child";
+    } else {
+        echo "your are an adult";
+    }
+    echo "</br>";
+
+    //php swich
+    $age1 = 35;
+    switch ($age1) {
+        case $age1 <= 10:
+            echo "you are a child";
+            break;
+        case $age1 <= 18:
+            echo "you are in teenage";
+            break;
+        case $age1 > 18 && $age1 < 40:
+            echo "you are an adult person";
+            break;
+        default:
+            echo "your are an old person";
+    }
+    echo "</br>";
+
+    // while loop
+    $number1 = 0;
+    while ($number1 <= 6) {
+        echo "You number is: $number1";
+        $number1++;
+        echo "</br>";
+    }
+    echo "</br>";
+
+    //for loop
+    for($i = 0; $i<=10; $i++){
+        echo "my name is faisal $i </br>";
+    }
+    echo "</br>";
+
+    //foreach loop
+    $color = array("red", "green", "blue", "white", "black");
+    foreach($color as $color){
+        echo "$color </br>";
+    }
+
+
     ?>
 </body>
 
